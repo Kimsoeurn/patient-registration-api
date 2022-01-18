705f2b002f54ac5900d52a94f75f86827752aeef
Kimsoeurn <sokkimsoeurn@gmail.com>
2022-01-18 16:05:35 +0700

    CRUD API Patients

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -2,33 +2,63 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Contracts\Services\IPatientService;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePatientRequest;
+use App\Http\Requests\UpdatePatientRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    public function index()
+    public function __construct(protected IPatientService $patientService)
     {
-        //
     }
 
-    public function store(Request $request)
+    public function index(): JsonResponse
     {
-        //
+        $data = $this->patientService->paginate();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+
+    public function store(CreatePatientRequest $request)
+    {
+        $patient = $this->patientService->insert($request);
+
+        return response()->json([
+            'status' => true,
+            'data' => $patient
+        ]);
     }
 
     public function show($id)
     {
-        //
+        $patient = $this->patientService->find($id);
+
+        return response()->json($patient);
     }
 
-    public function update(Request $request, $id)
+    public function update(UpdatePatientRequest $request, $id)
     {
-        //
+        $patient = $this->patientService->update($request, $id);
+
+        return response()->json([
+            'status' => true,
+            'data' => $patient,
+        ]);
     }
 
     public function destroy($id)
     {
-        //
+        $this->patientService->delete($id);
+
+        return response()->json([
+            'status' => true,
+            'data' => __('Deleted'),
+        ]);
     }
 }
---
+++ app/Services/PatientService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\Repositories\IPatientRepository;
+use App\Contracts\Services\IPatientService;
+use Illuminate\Http\Request;
+
+class PatientService implements IPatientService
+{
+    public function __construct(protected IPatientRepository $patientRepository)
+    {
+    }
+
+    public function all()
+    {
+        return $this->patientRepository->all();
+    }
+
+    public function paginate()
+    {
+       return $this->patientRepository->paginate();
+    }
+
+    public function find(int $id)
+    {
+        return $this->patientRepository->find($id);
+    }
+
+    public function findWhere(array $where, array $columns = ['*'])
+    {
+        // TODO: Implement findWhere() method.
+    }
+
+    public function insert(Request $request)
+    {
+        return $this->patientRepository->create($request->validated());
+    }
+
+    public function update(Request $request, int $id)
+    {
+        return $this->patientRepository->update($request->validated(), $id);
+    }
+
+    public function delete(int $id)
+    {
+        $this->patientRepository->delete($id);
+    }
+}
