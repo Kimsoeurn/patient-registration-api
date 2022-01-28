01b1e4ac0516424b1bbc6744c3f5ae02e2af3405
Kimsoeurn <sokkimsoeurn@gmail.com>
2022-01-28 17:34:21 +0700

    Update composer

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'frontLogin']);
 
 Route::middleware(['auth:api'])->group(function () {
-
+    Route::get('user', function () {
+        return auth()->user();
+    });
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::resource('patients', PatientController::class);
 });
 
-Route::resource('patients', PatientController::class);
 
 Route::get('provinces', [ProvinceController::class, 'index']);
 Route::get('provinces/{id}', [ProvinceController::class, 'show']);
